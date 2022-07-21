55c5cc26c59e9cb71251810dd32829375ebd4e11
Shawn Contant <shawnc366@gmail.com>
2022-07-21 17:07:25 -0500

    Inheritance and file I/O

--- Main.java
+++ Main.java
@@ -1,4 +1,58 @@
 import java.util.Scanner;
+import java.io.*;
+
+interface Demonstration {
+    void announceName();
+    void demonstrate(); // require they all have this method
+}
+
+class FileWriteDemonstration implements Demonstration {
+    static String getName() {
+        return "File writer";
+    }
+
+    public void announceName() {
+        System.out.println("*** " + getName() + " ***");
+    }
+
+    public void demonstrate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a line to save to file:");
+        String text = scanner.nextLine();
+        try {
+            FileWriter file = new FileWriter("file.txt");
+            file.write(text);
+            file.close();
+        } catch (IOException e) {
+            System.out.println("There was a problem writing to the file.");
+            e.printStackTrace();
+        }
+    }
+}
+
+class AreaDemonstration implements Demonstration {
+    static String getName() {
+        return "Area calculator";
+    }
+
+    public void announceName() {
+        System.out.println("=== " + getName() + " ===");
+    }
+
+    public void demonstrate() {
+        announceName();
+        System.out.println("Enter shape (circle, square):");
+        Scanner scanner = new Scanner(System.in);
+        String shape = scanner.nextLine();
+        AreaCalculator calculator = new AreaCalculator(shape);
+        try {
+            double area = calculator.calculate();
+            System.out.println("Area: " + area);
+        } catch (InvalidShapeException e) {
+            System.out.println("Invalid shape type!");
+        }
+    }
+}
 
 class InvalidShapeException extends Exception {
     public InvalidShapeException() {
@@ -38,16 +92,24 @@ class AreaCalculator {
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Pick a demonstration:");
+        System.out.println("(1) " + AreaDemonstration.getName());
+        System.out.println("(2) " + FileWriteDemonstration.getName());
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter shape (circle, square):");
-        String shape = scanner.nextLine();
-        AreaCalculator calculator = new AreaCalculator(shape);
-        try {
-            double area = calculator.calculate();
-            System.out.println("Area: " + area);
-        } catch (InvalidShapeException e) {
-            System.out.println("Invalid shape type!");
+        final int CHOICE_COUNT = 2;
+        int choice = scanner.nextInt();
+        if (choice < 1 || choice > CHOICE_COUNT) {
+            throw new Exception("Invalid number");
+        } else {
+            if (choice == 1) {
+                AreaDemonstration demo = new AreaDemonstration();
+                demo.demonstrate();
+            } else if (choice == 2) {
+                FileWriteDemonstration demo = new FileWriteDemonstration();
+                demo.demonstrate();
+            }
         }
     }
 }
